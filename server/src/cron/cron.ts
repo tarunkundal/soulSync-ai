e52c8b6c09a51533d5c521b2e6e5428ccbbb61b4
Tarun Chauhan <chauhan271singh@gmail.com>
2026-03-11 16:23:15 +0530

    changes the cron time for testing

--- server/src/cron/cron.ts
+++ server/src/cron/cron.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { sendTodayEventMessages } from "../ai/sendTodayEventMessages.js";
 
-cron.schedule("0 6 * * *", async () => {
+cron.schedule("26 16 * * *", async () => {
     await sendTodayEventMessages()
 },
     { timezone: "Asia/Kolkata", }

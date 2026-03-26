53def3106662fd5cd08cca300fdbc335d59ae1af
Tarun Chauhan <chauhan271singh@gmail.com>
2026-03-26 18:06:56 +0530

    change cron time

--- server/src/cron/cron.ts
+++ server/src/cron/cron.ts
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import { sendTodayEventMessages } from "../ai/sendTodayEventMessages.js";
 
-cron.schedule("5 18 * * *", async () => {
+cron.schedule("15 18 * * *", async () => {
     await sendTodayEventMessages()
 },
     { timezone: "Asia/Kolkata", }
